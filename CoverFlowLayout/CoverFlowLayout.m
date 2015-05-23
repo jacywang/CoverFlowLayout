13e07006dbf3f51e68c002111ee979f15fffcb7d
Jacy Wang <jacywj@gmail.com>
2015-05-23 13:20:21 -0700

    Finish Step 4

--- CoverFlowLayout/CoverFlowLayout.m
+++ CoverFlowLayout/CoverFlowLayout.m
@@ -14,9 +14,42 @@
     [super prepareLayout];
     
     self.scrollDirection = UICollectionViewScrollDirectionHorizontal;
-    self.itemSize = CGSizeMake(150, 100);
+    self.minimumLineSpacing = 10;
+    self.itemSize = CGSizeMake((self.collectionView.bounds.size.width - 4 * self.minimumLineSpacing) / 3, self.collectionView.bounds.size.height - 100);
+
 }
 
+-(NSArray *)layoutAttributesForElementsInRect:(CGRect)rect {
+    NSMutableArray *allAttributes = [[super layoutAttributesForElementsInRect:rect] mutableCopy];
+    
+    CGRect visibleRegion;
+    visibleRegion.origin = self.collectionView.contentOffset;
+    visibleRegion.size = self.collectionView.bounds.size;
+    
+    // Modify the layout attributes as needed here
+    for (UICollectionViewLayoutAttributes *attributes in allAttributes) {
+        attributes.transform3D = CATransform3DMakeScale(1, 0.8, 2);
+        float distance = abs(attributes.center.x - visibleRegion.origin.x - visibleRegion.size.width / 2);
+        if (distance < self.itemSize.width / 2) {
+            attributes.alpha = 1;
+        } else {
+            attributes.alpha = (self.itemSize.width / 2) / distance;
+        }
+        
+//        NSLog(@"attibutes.size.width:%f", attributes.bounds.size.width);
+//        NSLog(@"attributes.center.x: %f", attributes.center.x);
+//        NSLog(@"visibleRegion.origin.x: %f", visibleRegion.origin.x);
+//        NSLog(@"visibleRegion.size.width/2: %f", visibleRegion.size.width/2);
+//        NSLog(@"%d", abs(attributes.center.x - visibleRegion.origin.x - visibleRegion.size.width / 2))
+//        ;
+//        NSLog(@"alpha: %f", attributes.alpha);
+    }
+    
+    return allAttributes;
+}
 
+-(BOOL)shouldInvalidateLayoutForBoundsChange:(CGRect)newBounds {
+    return YES;
+}
 
 @end
