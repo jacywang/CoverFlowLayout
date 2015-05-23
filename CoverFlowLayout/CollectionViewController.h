cff48adef30521a3b74406e27548ff7324bdc633
Jacy Wang <jacywj@gmail.com>
2015-05-22 19:19:29 -0700

    Finish Step 1

--- CoverFlowLayout/CollectionViewController.h
+++ CoverFlowLayout/CollectionViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  CollectionViewController.h
 //  CoverFlowLayout
 //
 //  Created by JIAN WANG on 5/22/15.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface CollectionViewController : UICollectionViewController
 
 @end
-
