623dcca0474e0f1e81ee61a4d32cbd7342667702
Jacy Wang <jacywj@gmail.com>
2015-05-22 19:38:46 -0700

    Finish Step 2

---
+++ CoverFlowLayout/CoverFlowLayout.h
@@ -0,0 +1,13 @@
+//
+//  CoverFlowLayout.h
+//  CoverFlowLayout
+//
+//  Created by JIAN WANG on 5/22/15.
+//  Copyright (c) 2015 JWANG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CoverFlowLayout : UICollectionViewFlowLayout
+
+@end
---
+++ CoverFlowLayout/CoverFlowLayout.m
@@ -0,0 +1,13 @@
+//
+//  CoverFlowLayout.m
+//  CoverFlowLayout
+//
+//  Created by JIAN WANG on 5/22/15.
+//  Copyright (c) 2015 JWANG. All rights reserved.
+//
+
+#import "CoverFlowLayout.h"
+
+@implementation CoverFlowLayout
+
+@end
