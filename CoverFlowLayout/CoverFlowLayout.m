5bb05101ea1051e3d2cf0dee355652ef0ef4c6c6
Jacy Wang <jacywj@gmail.com>
2015-05-23 14:03:59 -0700

    Finish Step 5

--- CoverFlowLayout/CoverFlowLayout.m
+++ CoverFlowLayout/CoverFlowLayout.m
@@ -14,8 +14,8 @@
     [super prepareLayout];
     
     self.scrollDirection = UICollectionViewScrollDirectionHorizontal;
-    self.minimumLineSpacing = 10;
-    self.itemSize = CGSizeMake((self.collectionView.bounds.size.width - 4 * self.minimumLineSpacing) / 3, self.collectionView.bounds.size.height - 100);
+    self.minimumLineSpacing = 0;
+    self.itemSize = CGSizeMake(self.collectionView.bounds.size.width / 3, self.collectionView.bounds.size.height / 2 );
 
 }
 
@@ -28,13 +28,17 @@
     
     // Modify the layout attributes as needed here
     for (UICollectionViewLayoutAttributes *attributes in allAttributes) {
-        attributes.transform3D = CATransform3DMakeScale(1, 0.8, 2);
         float distance = abs(attributes.center.x - visibleRegion.origin.x - visibleRegion.size.width / 2);
         if (distance < self.itemSize.width / 2) {
             attributes.alpha = 1;
         } else {
             attributes.alpha = (self.itemSize.width / 2) / distance;
         }
+        
+        attributes.transform3D = CATransform3DMakeScale(1, 10, 2);
+        attributes.transform3D = CATransform3DMakeRotation(distance / 5 * M_PI / 180, 0.0, 1.0, 0.0);
+        
+        
         
 //        NSLog(@"attibutes.size.width:%f", attributes.bounds.size.width);
 //        NSLog(@"attributes.center.x: %f", attributes.center.x);
